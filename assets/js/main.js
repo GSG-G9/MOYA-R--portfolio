cb336931407562acfa8f469e45e66e3b4c126ff9
mAbadsa <moh22389@gmail.com>
2020-10-27 21:49:39 +0200

    Add Side Menu

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const $openMenuBtn = document.getElementById("open-menu");
+const $collapseNavbar = document.getElementsByClassName("collapse-navbar--container")[0];
+
+$openMenuBtn.addEventListener("click", (e) => {
+    if(!$collapseNavbar.classList.contains("open")) {
+        $collapseNavbar.classList.add("open");
+    } else {
+        $collapseNavbar.classList.remove("open")
+    }
+})
